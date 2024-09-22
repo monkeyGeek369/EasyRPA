869f36daaa87763d42bb6fa9b194f9a3839b2b0e
monkeygeek <monkeygeek@qq.com>
2024-09-22 22:10:18 +0800

    deleted:    flow/flow_task.py

--- flow/flow_task.py
+++
@@ -1,5 +0,0 @@
-from easyrpa.models.flow.flow_task_subscribe_dto import FlowTaskSubscribeDTO
-from easyrpa.models.flow.flow_task_subscribe_result_dto import FlowTaskSubscribeResultDTO
-
-def flow_task_create(dto:FlowTaskSubscribeDTO) -> FlowTaskSubscribeResultDTO:
-    return None
