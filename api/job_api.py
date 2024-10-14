9338fe24c3a94494f5d1166758ed75384db86261
monkeygeek <monkeygeek@qq.com>
2024-10-14 23:30:31 +0800

    new file:   api/job_api.py 	modified:   application/flask_app.py 	modified:   app.py

---
+++ api/job_api.py
@@ -0,0 +1,27 @@
+from flask import Blueprint,request
+from easyrpa.tools import str_tools,logs_tool,number_tool
+from easyrpa.tools.request_tool import easyrpa_request_wrapper
+from easyrpa.models.easy_rpa_exception import EasyRpaException
+from easyrpa.enums.easy_rpa_exception_code_enum import EasyRpaExceptionCodeEnum
+from database.dispatch_job_db_manager import DispatchJobDBManager
+from job import dispatch_job_manager
+
+
+job_api_bp =  Blueprint('job_api',__name__)
+
+@job_api_bp.route('/job/exe', methods=['POST'])
+@easyrpa_request_wrapper
+def flow_task_result_handler(job_id:int) -> bool:
+    if number_tool.num_is_empty(job_id):
+        raise EasyRpaException("job id is empty",EasyRpaExceptionCodeEnum.DATA_NULL.value[1],None,None)
+    
+    # search job
+    job = DispatchJobDBManager.get_dispatch_job_by_id(id=job_id)
+    if job is None:
+        raise EasyRpaException("job not found",EasyRpaExceptionCodeEnum.DATA_NOT_FOUND.value[1],None,job_id)
+
+    # exe
+    job_type_abc = dispatch_job_manager.get_job_type_impl(job_type=job.job_type)
+    job_type_abc.execute_job(job=job)
+
+    return True
--- application/flask_app.py
+++ application/flask_app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from api.flow_task_api import flow_task_bp
+from api.job_api import job_api_bp
 
 # 注册flask应用
 app = Flask(__name__)
 
 # 注册蓝图
 app.register_blueprint(flow_task_bp)
+app.register_blueprint(job_api_bp)
